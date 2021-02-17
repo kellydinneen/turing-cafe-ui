004dddbf9adc28c5e3458de17bbbd75862f3679a
kellydinneen <kgdinneen@gmail.com>
2021-02-17 09:46:59 -0700

    Create POST and DELETE requests

--- src/Form.js
+++ src/Form.js
@@ -1,39 +1,54 @@
 import React from 'react';
 import './Form.css';
 
-const Form = (props) => {
+class Form extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: ''
+    }
+  }
 
-  const {name, date, time, number} = props.reservation;
+  updateForm = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
 
-  return (
-    <form className="reservation-card">
-      <input type='text'
-            placeholder='Name'
-            name='name'
-            value={name}
-            onChange={this.updateForm}>
-      </input>
-      <input type='text'
-            placeholder='Date (mm/dd)'
-            name='date'
-            value={date}
-            onChange={this.updateForm}>
-      </input>
-      <input type='text'
-            placeholder='Time (hh:mm am/pm)'
-            name='time'
-            value={time}
-            onChange={this.updateForm}>
-      </input>
-      <input type='text'
-            placeholder='Number of guests'
-            name='number'
-            value={number}
-            onChange={this.updateForm}>
-      </input>
-      <button>Make Reservation</button>
-    </form>
-  )
+  render() {
+    
+    return (
+      <form className="reservation-card">
+        <input type='text'
+              placeholder='Name'
+              name='name'
+              value={name}
+              onChange={this.updateForm}>
+        </input>
+        <input type='text'
+              placeholder='Date (mm/dd)'
+              name='date'
+              value={date}
+              onChange={this.updateForm}>
+        </input>
+        <input type='text'
+              placeholder='Time (hh:mm am/pm)'
+              name='time'
+              value={time}
+              onChange={this.updateForm}>
+        </input>
+        <input type='text'
+              placeholder='Number of guests'
+              name='number'
+              value={number}
+              onChange={this.updateForm}>
+        </input>
+        <button>Make Reservation</button>
+      </form>
+    )
+  }
+
 }
 
 export default Reservation;
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,3 +7,30 @@ export const fetchReservations = () => {
         return res.json()
       }})
   }
+
+  export const addReservation = (reservation) => {
+    const post = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(reservation)
+      }
+    return fetch(`http://localhost:3001/api/v1/reservations`, post)
+      .then(res => {if (!res.ok) {
+          return `${res.status} error. Sorry! Something went wrong! Try again later or go to Contact Us to contact the developers with questions!`;
+        } else {
+          return res.json();
+        }})
+  }
+
+  export const cancelReservation = (reservationID) => {
+    const delete = {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+      }
+    return fetch(`http://localhost:3001/api/v1/reservations/${reservationID}`, delete)
+      .then(res => {if (!res.ok) {
+          return `${res.status} error. Sorry! Something went wrong! Try again later or go to Contact Us to contact the developers with questions!`;
+        } else {
+          return res.json();
+        }})
+  }
